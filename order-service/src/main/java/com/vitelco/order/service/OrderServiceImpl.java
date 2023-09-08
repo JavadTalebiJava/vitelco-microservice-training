65aaedafd78e1162ec41d197743410c69b108049
kafeinjtalebi <javad.talebi2@vodafone.com>
2023-09-08 23:30:56 +0300

    add keycloak and SecurityConfig in gateway

--- order-service/src/main/java/com/vitelco/order/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/vitelco/order/service/OrderServiceImpl.java
@@ -10,7 +10,6 @@ import com.vitelco.order.model.dto.response.OrderResponse;
 import com.vitelco.order.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
